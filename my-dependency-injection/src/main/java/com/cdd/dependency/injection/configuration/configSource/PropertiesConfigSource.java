964a6fcc39c3ccae8a4c43017b0b496db548f166
yangfengshan3 <yangfengshan2@jd.com>
2021-03-24 20:18:29 +0800

    错对对 《新增功能》 ##拆分功能模块  servlet 采用代码方式注入到上下文中 了解怎么去除web.xml

--- my-dependency-injection/src/main/java/com/cdd/dependency/injection/configuration/configSource/PropertiesConfigSource.java
+++ my-dependency-injection/src/main/java/com/cdd/dependency/injection/configuration/configSource/PropertiesConfigSource.java
@@ -1,46 +1,31 @@
 package com.cdd.dependency.injection.configuration.configSource;
 
-import org.eclipse.microprofile.config.spi.ConfigSource;
+import com.cdd.geekbanglessons.configuration.microprofile.config.source.MapBasedConfigSource;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
 import java.util.Properties;
-import java.util.Set;
-import java.util.stream.Collectors;
 
-public class PropertiesConfigSource implements ConfigSource {
+public class PropertiesConfigSource extends MapBasedConfigSource {
 
-    /**
-     * Java 系统属性最好通过本地变量保存，使用 Map 保存，尽可能运行期不去调整
-     * -Dapplication.name=user-web
-     */
-    private final Properties properties = new Properties();
+    protected PropertiesConfigSource() {
+        super("PropertiesConfigSource", 400);
+    }
 
-    public PropertiesConfigSource() {
+    @Override
+    protected void prepareConfigData(Map configData) throws Throwable {
+        Properties properties = new Properties();
         //获取properties文件
         // 使用ClassLoader加载properties配置文件生成对应的输入流
         InputStream in = PropertiesConfigSource.class.getClassLoader().getResourceAsStream("META-INF/config/config.properties");
         // 使用properties对象加载输入流
-        if (null==in)return;
+        if (null == in) return;
         try {
             properties.load(in);
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    @Override
-    public Set<String> getPropertyNames() {
-        return properties.keySet().stream().map(Object::toString).collect(Collectors.toSet());
-    }
-
-    @Override
-    public String getValue(String propertyName) {
-        return properties.getProperty(propertyName);
-    }
-
-    @Override
-    public String getName() {
-        return "config/config.properties";
+        configData.putAll(properties);
     }
 }
